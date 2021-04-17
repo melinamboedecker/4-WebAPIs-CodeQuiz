992f162e4fee8b2edf0989f54a910625a79dcb3d
Melina Boedecker <melinamboedecker@gmail.com>
2021-04-16 20:15:43 -0700

    Add display of correct or incorrect after each question is answered

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -19,7 +19,7 @@ var startQuizBtn = document.createElement("button")
 // var answerBtn2 = document.querySelector("#answerbutton2");
 // var answerBtn3 = document.querySelector("#answerbutton3");
 
- var score = 0;
+var score = 0;
 
 
 // var test = document.createElement("p");
@@ -33,12 +33,12 @@ var startQuizBtn = document.createElement("button")
 var allQuestions = [
     {
         question: "Which of the following IS NOT a data type?",
-        answers: ['boolean', 'string','object', 'method'],
-        correctAnswer: 'object'
+        answers: ['boolean', 'string', 'object', 'method'],
+        correctAnswer: 'method'
     },
     {
         question: "How many argumennts does the setInterval() function take?",
-        answers: ['one', 'two','three', 'none'],
+        answers: ['one', 'two', 'three', 'none'],
         correctAnswer: 'two'
     },
     {
@@ -55,12 +55,12 @@ console.log(totalQuestions);
 var counter = 0;
 
 var questionOrder = [];
-    for (var i=0; i < allQuestions.length; i++) {
-        questionOrder.push(i);
-    }
-    
-    console.log(questionOrder);
-    console.log(questionOrder.length);
+for (var i = 0; i < allQuestions.length; i++) {
+    questionOrder.push(i);
+}
+
+console.log(questionOrder);
+console.log(questionOrder.length);
 
 // function shuffle(arraytoShuffle) {
 //     let counter = arraytoShuffle.length;
@@ -84,7 +84,7 @@ var questionOrder = [];
 
 
 
-function init () {
+function init() {
     timerText.style.display = "none";
     h1El.textContent = "Coding Quiz Challenge";
     headerElement.appendChild(h1El);
@@ -102,10 +102,10 @@ function startQuiz() {
 }
 
 function startTimer() {
-    timer = setInterval(function() {
+    timer = setInterval(function () {
         timerCount--;
         timerElement.textContent = timerCount;
-        if (timerCount === 0) {
+        if (timerCount < 1) {
             clearInterval(timer);
             calcScore();
         }
@@ -115,53 +115,66 @@ function startTimer() {
 function startQuestions(event) {
     topP.style.display = "none";
     startQuizBtn.style.display = "none";
-    
+
     console.log(allQuestions);
     headerElement.textContent = allQuestions[counter].question;
     console.log(allQuestions[counter].answers);
-    
-        for (var i = 0; i < allQuestions[counter].answers.length; i++) {
-            var temp = document.createElement("section");
-            temp.className = "ansbutton-section";
-            temp.textContent = allQuestions[counter].answers[i];
-            buttonContainer.appendChild(temp);
-            answerButtons = document.querySelectorAll(".ansbutton-section");
-        }
 
-        console.log(answerButtons.length);
-        console.log(answerButtons);
-        for (var a=0; a < answerButtons.length; a++) {
-            answerButtons[a].addEventListener("click", function(event) {
-                console.log(this.textContent);
-                if (this.textContent === allQuestions[counter].correctAnswer) {
-                    console.log('Correct');
-                    score = score + 10;
-                } else {
-                    console.log('Incorrect');
-                }
-                console.log(score);
-            
+    for (var i = 0; i < allQuestions[counter].answers.length; i++) {
+        var temp = document.createElement("section");
+        temp.className = "ansbutton-section";
+        temp.textContent = allQuestions[counter].answers[i];
+        buttonContainer.appendChild(temp);
+        answerButtons = document.querySelectorAll(".ansbutton-section");
+    }
+
+    console.log(answerButtons.length);
+    console.log(answerButtons);
+    for (var a = 0; a < answerButtons.length; a++) {
+        answerButtons[a].addEventListener("click", function (event) {
+            console.log(this.textContent);
+            if (this.textContent === allQuestions[counter].correctAnswer) {
+                bottomP.textContent = "Correct!";
+                bottomP.setAttribute("style", "border-top: 2px solid black");
+                score = score + 10;
+                setTimeout(function(){
+                    bottomP.textContent = "";
+                    bottomP.setAttribute("style", "border-top:none");
+                }, 2000); 
+
+            } else {
+                timerCount = timerCount - 10;
+                bottomP.textContent = 'Incorrect';
+                bottomP.setAttribute("style", "border-top: 2px solid black");
+                setTimeout(function(){
+                    bottomP.textContent = "";
+                    bottomP.setAttribute("style", "border-top:none");
+                }, 2000); 
+            }
+            console.log(score);
+
             console.log(answerButtons.length);
-            for (var r = 0; r<answerButtons.length; r++) {
+            for (var r = 0; r < answerButtons.length; r++) {
                 var remove = document.querySelector(".ansbutton-section");
                 remove.parentElement.removeChild(remove);
             }
-            
-            
-                
-            nextQuestion();            
+
+
+
+            nextQuestion();
         })
-    
-        };
-    
+
+    };
+
 }
 
 function nextQuestion() {
     console.log('nextquestion');
-    counter = counter +1;
+    counter = counter + 1;
     console.log(counter);
     if (counter >= allQuestions.length) {
         calcScore();
+        timerCount = 0;
     } else {
         headerElement.textContent = allQuestions[counter].question;
         for (var i = 0; i < allQuestions[counter].answers.length; i++) {
@@ -174,24 +187,35 @@ function nextQuestion() {
 
         console.log(answerButtons.length);
         console.log(answerButtons);
-        for (var a=0; a < answerButtons.length; a++) {
-            answerButtons[a].addEventListener("click", function(event) {
+        for (var a = 0; a < answerButtons.length; a++) {
+            answerButtons[a].addEventListener("click", function (event) {
                 console.log(this.textContent);
                 if (this.textContent === allQuestions[counter].correctAnswer) {
-                    console.log('Correct');
+                    bottomP.textContent = "Correct!"
+                    bottomP.setAttribute("style", "border-top: 2px solid black");
                     score = score + 10;
+                    setTimeout(function(){
+                        bottomP.textContent = "";
+                        bottomP.setAttribute("style", "border-top:none");
+                    }, 2000); 
                 } else {
-                    console.log('Incorrect');
+                    timerCount = timerCount - 10;
+                    bottomP.textContent = "Incorrect";
+                    bottomP.setAttribute("style", "border-top: 2px solid black");
+                    setTimeout(function(){
+                        bottomP.textContent = "";
+                        bottomP.setAttribute("style", "border-top:none");
+                    }, 2000); 
                 }
                 console.log(score);
 
-            for (var r = 0; r<answerButtons.length; r++) {
-                var remove = document.querySelector(".ansbutton-section");
-                remove.parentElement.removeChild(remove);
-            }    
-            nextQuestion();            
-        })
-    
+                for (var r = 0; r < answerButtons.length; r++) {
+                    var remove = document.querySelector(".ansbutton-section");
+                    remove.parentElement.removeChild(remove);
+                }
+                nextQuestion();
+            })
+
         };
     }
 }
@@ -199,13 +223,13 @@ function nextQuestion() {
 function calcScore() {
     console.log("YAY");
     console.log(score);
-    headerElement.style.display = "none";    
+    //put initials and score into local memory
+    headerElement.style.display = "none";
 }
 
 function highScores() {
     header.style.display = "none";
 }
-
 
 function demoQuiz() {
     console.log("demo");
@@ -225,7 +249,7 @@ function demoQuiz() {
         buttonContainer.appendChild(temp);
     }
     var answerButtons = document.querySelector(".ansbutton");
-    answerButtons.addEventListener("click", function(event) {
+    answerButtons.addEventListener("click", function (event) {
         alert(this.id);
     });
 }
